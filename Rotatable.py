3e8aab79c08f2518c129369409f1674b300e4170
Jefferylee Li <jeffreylee.LI@nio.com>
2024-09-24 17:41:28 +0800

    Rotatable.py补充提交

--- Rotatable.py
+++ Rotatable.py
@@ -5,7 +5,7 @@ import typing
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 import ZMQClient
-from Utils import get_door_handle_stat
+from Utils import get_door_handle_stat, get_conor_signal_group
 from ZMQClient import ZmqClient
 
 
@@ -13,6 +13,8 @@ class SwitchSignalListItem(QtWidgets.QListWidgetItem):
     """
     用于发送开关信号的Item按钮，点击后可发送SOA信号，反转信号状态和Item样式
     """
+    mapping_rule_dict = {'车门':{2:'on', 1:'off'}, '车窗':{100:'on', 0:'off'}, '把手':{1:'on', 2:'off'}}
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setFont(QtGui.QFont("Alibaba PuHuiTi", 12))
@@ -20,6 +22,7 @@ class SwitchSignalListItem(QtWidgets.QListWidgetItem):
         self.signal_name = self.text()
         self.is_on = False
         self.setForeground(QtGui.QBrush(QtGui.QColor('gray')))
+
 
     def click(self, area=None):
         print(f"Area Id:{area}")
@@ -30,6 +33,23 @@ class SwitchSignalListItem(QtWidgets.QListWidgetItem):
         self.setText(self.signal_name + f'({sgnl_sts_sfx})')
         print(f'{self.text()} : {self.is_on}')
 
+    def style_on(self):
+        self.is_on = True
+        self.setForeground(self.default_bg)
+        self.setText(self.signal_name + '已开启')
+
+    def style_off(self):
+        self.is_on = False
+        self.setForeground(QtGui.QBrush(QtGui.QColor('gray')))
+        self.setText(self.signal_name + '已关闭')
+
+    def style_invalid(self):
+        self.is_on = False
+        self.setForeground(QtGui.QBrush(QtGui.QColor('red')))
+        self.setText(self.signal_name + '异常')
+
+
+mapping_rule_dict = {'车门': {2: 'on', 1: 'off'}, '车窗': {100: 'on', 0: 'off'}, '把手': {1: 'on', 2: 'off'}}
 
 class SwitchSignalListArea(QtWidgets.QListWidget):
     """
@@ -38,9 +58,10 @@ class SwitchSignalListArea(QtWidgets.QListWidget):
     def __init__(self, *args, size=QtCore.QRect(), signal_list=None, bind_choice = None, **kwargs, ):
         super().__init__(*args, **kwargs)
         if bind_choice is not None:
-            bind_choice.bind_signal_list = self
+            bind_choice.bind_QListWidget = self
         self.bind_choice = bind_choice
         self.itemClicked.connect(self.clicked)
+        self.signal_stat_dict = {}
         self.setGeometry(size)
         self.addItems(signal_list)
 
@@ -54,12 +75,19 @@ class SwitchSignalListArea(QtWidgets.QListWidget):
         for text in labels:
             switch_item = SwitchSignalListItem(text)
             super(SwitchSignalListArea, self).addItem(switch_item)
+            self.signal_stat_dict[text] = switch_item
 
     def addItem(self, aitem: SwitchSignalListItem) -> None:
         super().addItem(aitem)
 
-    # def update_signal_stat_style(self, area: int):
-
+    def update_signal_stat_style(self, stat_dict:dict[str, int]) -> None:
+        for key, value in stat_dict.items():
+            if mapping_rule_dict[key].get(value, 'invalid') == 'on':
+                self.signal_stat_dict[key].style_on()
+            elif mapping_rule_dict[key].get(value, 'invalid') == 'off':
+                self.signal_stat_dict[key].style_off()
+            else:
+                self.signal_stat_dict[key].style_invalid()
 
 
 
@@ -69,7 +97,7 @@ class AreaChoice(QtWidgets.QComboBox):
     """
     def __init__(self,*args, size=None, choice_list=None,**kwargs):
         super().__init__(*args,**kwargs)
-        self.bind_signal_list = None
+        self.bind_QListWidget = None
         self.check_box_font = QtGui.QFont("Alibaba PuHuiTi", 18)
         self.setFont(self.check_box_font)
         self.setEditable(True)
@@ -78,13 +106,11 @@ class AreaChoice(QtWidgets.QComboBox):
         self.currentIndexChanged.connect(self.update_bind_signal_stat)
 
 
-    def update_bind_signal_stat(self):
-        """根据Area Id获取当前ID下的车控信号状态,并更新GUI信号状态"""
-        if self.bind_signal_list is not None:
-            stat_list = get_door_handle_stat()
-
-
-
+    def update_bind_signal_stat(self, area_index):
+        """根据Area Id获取当前Conor下的车控信号状态,并更新该组信号列表状态"""
+        if self.bind_QListWidget is not None:
+            stat_dict = get_conor_signal_group(area_index)
+            self.bind_signal_stat.update_signal_stat_style(stat_dict)
 
 
 class Rotatable(object):
